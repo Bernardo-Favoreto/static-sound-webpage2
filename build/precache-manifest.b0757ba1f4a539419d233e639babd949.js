e09e8d07dbe2dd06ad1ea6947127fae6445f6a57
Bernardo-Favoreto <bernardo.pelisson@gmail.com>
2020-08-22 18:53:48 -0300

    Add build

---
+++ build/precache-manifest.b0757ba1f4a539419d233e639babd949.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbb419495dba23453b2a292512a999a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602c43440894d2d1ca9f",
+    "url": "/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "f0abb6c83dba28f6bc49",
+    "url": "/static/css/main.55075417.chunk.css"
+  },
+  {
+    "revision": "602c43440894d2d1ca9f",
+    "url": "/static/js/2.b2cf57d2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b2cf57d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0abb6c83dba28f6bc49",
+    "url": "/static/js/main.c76461fc.chunk.js"
+  },
+  {
+    "revision": "f3e3d39c6956ba81638d",
+    "url": "/static/js/runtime-main.730ad540.js"
+  },
+  {
+    "revision": "ca8c35c7b2f62a833f316e3546fc0259",
+    "url": "/static/media/bg2.ca8c35c7.jpg"
+  },
+  {
+    "revision": "c7b6deb5ae9fdeba0e77648db17d337d",
+    "url": "/static/media/logo-angelika-favorreto-pb.c7b6deb5.png"
+  },
+  {
+    "revision": "dc1c8289cf103f5d03c5af9c533176a9",
+    "url": "/static/media/meditacao2.dc1c8289.mp3"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "5a0b22e47a341e7d392526b1de51bfec",
+    "url": "/static/media/transparente-cropped.5a0b22e4.png"
+  }
+]);
